4e31bd678f5dc6009a77a608b7a70b2cf272d19b
genuinoLuiz <luizgustavo@pitom.com.br>
2025-03-09 11:20:33 -0400

    feat: configura pwa

---
+++ src/app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+    return (
+      <>
+        <title>Meu App PWA</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      </>
+    );
+  }
+  
